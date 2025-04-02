c2b6c61e310af0ce7109fb3f9bde659efc09aff8
daniele-aceti <daniele.aceti@hotmail.it>
2025-04-02 21:21:10 +0200

    feat: ex

---
+++ src/main/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,28 @@
+package pizzeria.spring_la_mia_pizzeria_crud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pizzeria.spring_la_mia_pizzeria_crud.model.Pizza;
+import pizzeria.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+
+@Controller
+@RequestMapping("/pizze")
+
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Pizza> result = pizzaRepository.findAll();
+        model.addAttribute("pizzaList", result);
+        return "index";
+    }
+}
---
+++ src/main/java/pizzeria/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,68 @@
+package pizzeria.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizze")//pizze è nome della mia tabella
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String nome;
+
+    private String descrizione;
+
+    @Column(nullable = false)
+    private String foto;
+
+    @Column(nullable = false)
+    private String prezzo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(String prezzo) {
+        this.prezzo = prezzo;
+    }
+}
