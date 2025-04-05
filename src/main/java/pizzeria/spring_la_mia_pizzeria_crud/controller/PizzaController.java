eb787564ff8e390ef5ca266f15a87a5c8b19fa60
daniele-aceti <daniele.aceti@hotmail.it>
2025-04-05 12:36:29 +0200

    feat: search

--- src/main/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pizzeria.spring_la_mia_pizzeria_crud.model.Pizza;
 import pizzeria.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
@@ -32,8 +33,13 @@ public class PizzaController {
     }
 
     @GetMapping("/pizze")
-    public String index(Model model) {
-        List<Pizza> result = pizzaRepository.findAll();
+    public String index(Model model,@RequestParam(name="keyword", required= false) String findPizza) {
+        List<Pizza> result;
+        if(findPizza != null && !findPizza.isBlank()){
+            result = pizzaRepository.findByNomeContainingIgnoreCase(findPizza);
+        }else{
+            result = pizzaRepository.findAll();
+        }
         model.addAttribute("pizzaList", result);
         return "pizze/index";
     }
